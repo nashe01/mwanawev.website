6c59e6934383e41355d1d97d13041dd27b02edb5
nashe01 <mupindu021@gmail.com>
2025-11-10 13:25:05 +0200

    style(hero): remove gradient overlay from hero video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,7 +16,6 @@ export const Hero = () => {
         >
           <source src={heroBgVideo} type="video/mp4" />
         </video>
-        <div className="absolute inset-0 bg-gradient-to-b from-black/70 via-black/50 to-black/70" />
       </div>
 
       {/* Content */}
